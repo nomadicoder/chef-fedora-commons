370ce52f5fe5e640d5f6a267ce8286edb20ad6f1
Steven Ng <steven@nomadicoder.com>
2013-12-31 15:39:07 -0800

    Add the deployer user

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -6,3 +6,16 @@ package "openjdk-7-jre"
 package "nodejs"
 package "imagemagick"
 package "graphicsmagick-libmagick-dev-compat"
+
+user node[:user][:name] do
+  password node[:user][:password]
+  group node[:user][:group]
+  home "/home/#{node[:user][:name]}"
+  supports manage_home: true
+  shell "/bin/bash"
+end
+
+template "/home/#{node[:user][:name]}.bashrc" do
+  source "bashrc.erb"
+  owner node[:user][:name]
+end
