32751715f54d8ffd9805936d7c2bf840b150728a
Steven Ng <steven@nomadicoder.com>
2013-12-31 20:13:19 -0800

    Install fits and redis

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -21,3 +21,33 @@ template "/home/#{node[:user][:name]}.bashrc" do
   source "bashrc.erb"
   owner node[:user][:name]
 end
+
+script "install_fits" do
+  interpreter "bash"
+  cwd "/opt"
+  user "root"
+  code <<-EOH
+  wget https://fits.googlecode.com/files/fits-0.6.2.zip
+  unzip fits-0.6.2.zip
+  cd fits-0.6.2
+  chmod +x fits.sh
+  EOH
+
+  not_if { ::File.executable?("/opt/fits-0.6.2/fits.sh") }
+end
+
+script "install_redis" do
+  interpreter "bash"
+  cwd "/usr/local/src"
+  user "root"
+  code <<-EOH
+  wget http://download.redis.io/releases/redis-2.8.3.tar.gz
+  tar -zxf redis-2.8.3.tar.gz
+  cd redis-2.8.3
+  sudo make
+  sudo make install
+  ls /usr/local/bin
+  EOH
+
+  not_if { ::File.exists?("/usr/local/bin/redis-server") }
+end
