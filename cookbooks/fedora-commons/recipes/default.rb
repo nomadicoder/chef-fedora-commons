a412628e3a7b693e303373299f0c3bfbff743ad1
Steven Ng <steven@nomadicoder.com>
2014-01-10 08:56:10 -0800

    using tomcat7 owner

--- cookbooks/fedora-commons/recipes/default.rb
+++ cookbooks/fedora-commons/recipes/default.rb
@@ -12,14 +12,17 @@ script "update_environment" do
   cwd "/etc"
   user "root"
   code <<-EOH
-  if ! grep -Fq "JAVA_HOME=/usr" /etc/environment; then
-    echo "JAVA_HOME=/usr" >> /etc/environment
+  if ! grep -Fq "JAVA_HOME=#{node['fedora-commons'][:java_home]}" /etc/environment; then
+    echo "JAVA_HOME=#{node['fedora-commons'][:java_home]}" >> /etc/environment
   fi
-  if ! grep -Fq "FEDORA_HOME=/usr/local/fedora" /etc/environment; then
-    echo "FEDORA_HOME=/usr/local/fedora" >> /etc/environment
+  if ! grep -Fq "FEDORA_HOME=#{node['fedora-commons'][:fedora_home]}" /etc/environment; then
+    echo "FEDORA_HOME=#{node['fedora-commons'][:fedora_home]}" >> /etc/environment
   fi
-  if ! grep -Fq "CATALINA_HOME=/usr/local/fedora/tomcat" /etc/environment; then
-    echo "CATALINA_HOME=/usr/local/fedora/tomcat" >> /etc/environment
+  if ! grep -Fq "CATALINA_HOME=#{node['fedora-commons'][:catalina_home]}" /etc/environment; then
+    echo "CATALINA_HOME=#{node['fedora-commons'][:catalina_home]}" >> /etc/environment
+  fi
+  if ! grep -Fq "RAILS_ENV=#{node['fedora-commons'][:rails_env]}" /etc/environment; then
+    echo "RAILS_HOME=#{node['fedora-commons'][:rails_env]}" >> /etc/environment
   fi
   EOH
 end
@@ -65,64 +68,70 @@ mysql_database_user node['fedora-commons'][:database_username] do
   action        :grant
 end
 
-template "/home/#{node[:user][:name]}/install.properties" do
-  source "install.properties.erb"
-  owner node[:user][:name]
-  group node[:user][:name]
+directory "#{node['fedora-commons'][:installDir]}" do
+  owner  node['fedora-commons'][:tomcat_user]
+  group  node['fedora-commons'][:tomcat_user]
+  action :create
 end
 
 directory "#{node['fedora-commons'][:fedora_home]}" do
-  owner node[:user][:name]
-  group node[:user][:name]
+  owner  node['fedora-commons'][:tomcat_user]
+  group  node['fedora-commons'][:tomcat_user]
   action :create
 end
 
 execute "download_fedora" do
-  cwd "/home/#{node[:user][:name]}"
-  user node[:user][:name]
-  group node[:user][:name]
+  cwd    "#{node['fedora-commons'][:installDir]}"
+  user   node['fedora-commons'][:tomcat_user]
+  group  node['fedora-commons'][:tomcat_user]
   command "wget http://sourceforge.net/projects/fedora-commons/files/fedora/#{node['fedora-commons'][:version]}/fcrepo-installer-#{node['fedora-commons'][:version]}.jar"
 
   not_if { ::File.exists?("/home/#{node[:user][:name]}/fcrepo-installer-#{node['fedora-commons'][:version]}.jar") }
 end
 
+template "#{node['fedora-commons'][:installDir]}/install.properties" do
+  source "install.properties.erb"
+  owner  node['fedora-commons'][:tomcat_user]
+  group  node['fedora-commons'][:tomcat_user]
+end
+
 execute "install_fedora" do
-  user node[:user][:name]
-  group node[:user][:name]
-  cwd "/home/#{node[:user][:name]}"
+  cwd    "#{node['fedora-commons'][:installDir]}"
+  user   node['fedora-commons'][:tomcat_user]
+  group  node['fedora-commons'][:tomcat_user]
   command "java -jar fcrepo-installer-#{node['fedora-commons'][:version]}.jar install.properties"
 
-  not_if { ::File.exists?("#{node['fedora-commons'][:catalina_home]}") }
+  not_if { ::File.exists?("#{node['fedora-commons'][:fedora_home]}/server") }
 end
 
 execute "download_solr" do
-  cwd "/home/#{node[:user][:name]}"
-  user node[:user][:name]
-  group node[:user][:name]
+  cwd    "#{node['fedora-commons'][:installDir]}"
+  user   node['fedora-commons'][:tomcat_user]
+  group  node['fedora-commons'][:tomcat_user]
   command "wget http://psg.mtu.edu/pub/apache/lucene/solr/#{node[:solr][:version]}/solr-#{node[:solr][:version]}.tgz"
 
-  not_if { ::File.exists?("/home/#{node[:user][:name]}/solr-#{node[:solr][:version]}.tgz") }
+  not_if { ::File.exists?("#{node['fedora-commons'][:installDir]}/solr-#{node[:solr][:version]}.tgz") }
 end
 
 execute "extract_solr" do
-  cwd "/home/#{node[:user][:name]}"
-  user node[:user][:name]
-  group node[:user][:name]
+  cwd    "#{node['fedora-commons'][:installDir]}"
+  user   node['fedora-commons'][:tomcat_user]
+  group  node['fedora-commons'][:tomcat_user]
   command "tar -zxf solr-#{node[:solr][:version]}.tgz"
 
-  not_if { ::File.exists?("/home/#{node[:user][:name]}/solr-#{node[:solr][:version]}") }
+  not_if { ::File.exists?("#{node['fedora-commons'][:installDir]}/solr-#{node[:solr][:version]}") }
 end
 
 require 'etc'
-uid = Etc.getpwnam(node[:user][:name]).uid
+uid = Etc.getpwnam(node['fedora-commons'][:tomcat_user]).uid
 
 script "install_solr" do
   interpreter "bash"
-  cwd "/home/#{node[:user][:name]}"
+  cwd  "#{node['fedora-commons'][:installDir]}"
   user 'root'
   code <<-EOH
   cp -pr solr-#{node[:solr][:version]}/example/solr #{node[:solr][:home]}
-  chown -R #{node[:user][:name]}:#{node[:user][:name]} #{node[:solr][:home]}
+  chown -R #{node['fedora-commons'][:tomcat_user]}:#{node['fedora-commons'][:tomcat_user]} #{node[:solr][:home]}
   EOH
 
   not_if { ::File.exists?("#{node[:solr][:home]}") }
@@ -131,46 +140,51 @@ end
 
 script "make_solr_app" do
   interpreter "bash"
-  cwd "/home/#{node[:user][:name]}"
-  user node[:user][:name]
-  group node[:user][:name]
+  cwd   "#{node['fedora-commons'][:installDir]}"
+  user  node['fedora-commons'][:tomcat_user]
+  group node['fedora-commons'][:tomcat_user]
   code <<-EOH
-  tar -zxf solr-#{node[:solr][:version]}.tgz
   cp -pr solr-#{node[:solr][:version]}/dist/solr-#{node[:solr][:version]}.war #{node[:solr][:home]}/solr.war
-  cp -pr solr-#{node[:solr][:version]}/example/lib/ext/* #{node['fedora-commons'][:catalina_home]}/lib
-  cp -pr solr-#{node[:solr][:version]}/example/resources/log4j.properties #{node['fedora-commons'][:catalina_home]}/lib
+  #cp -pr solr-#{node[:solr][:version]}/example/lib/ext/* #{node['fedora-commons'][:catalina_home]}/lib
   EOH
 
   not_if { ::File.directory?("/#{node[:solr][:home]}/#{node[:solr][:home]}/solr.war") }
 end
 
+template "#{node['fedora-commons'][:catalina_home]}/lib/log4j.properties" do
+  source "log4j.properties.erb"
+  owner  node['fedora-commons'][:tomcat_user]
+  group  node['fedora-commons'][:tomcat_user]
+  owner 'tomcat7'
+end
+
 directory "#{node[:solr][:dataDir]}" do
-  owner node[:user][:name]
-  group node[:user][:name]
+  owner  node['fedora-commons'][:tomcat_user]
+  group  node['fedora-commons'][:tomcat_user]
   action :create
 end
 
 directory "#{node[:solr][:dataDir]}/data" do
-  owner node[:user][:name]
-  group node[:user][:name]
+  owner  node['fedora-commons'][:tomcat_user]
+  group  node['fedora-commons'][:tomcat_user]
   action :create
 end
 
 directory "#{node[:solr][:home]}/conf" do
-  owner node[:user][:name]
-  group node[:user][:name]
+  owner  node['fedora-commons'][:tomcat_user]
+  group  node['fedora-commons'][:tomcat_user]
   action :create
 end
 
 template "#{node[:solr][:home]}/conf/solrconfig.xml" do
   source "solrconfig.xml.erb"
-  owner node[:user][:name]
-  group node[:user][:name]
+  owner  node['fedora-commons'][:tomcat_user]
+  group  node['fedora-commons'][:tomcat_user]
 end
 
 template "#{node['fedora-commons'][:catalina_home]}/conf/Catalina/localhost/bl_solr.xml" do
   source "bl_solr.xml.erb"
-  owner node[:user][:name]
-  group node[:user][:name]
+  owner  node['fedora-commons'][:tomcat_user]
+  group  node['fedora-commons'][:tomcat_user]
 end
 
