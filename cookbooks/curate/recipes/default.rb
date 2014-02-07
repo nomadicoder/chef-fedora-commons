ef68339bad8cb33fa78495c12ca803a64eb6080f
Steven Ng <steven@nomadicoder.com>
2014-02-07 10:52:22 -0800

    use package ClamAV

--- cookbooks/curate/recipes/default.rb
+++ cookbooks/curate/recipes/default.rb
@@ -16,6 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+package "clamav"
+
 script "install_fits" do
   interpreter "bash"
   cwd "/opt"
